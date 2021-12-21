804231fbba7db2c32fbc34d9360d52f22446072f
Ileenfdz <ileenfdz@gmail.com>
2021-12-21 12:59:33 +0100

    Added new test in example, to try it, made it do an error and then, have a success

--- src/tests/example.test.js
+++ src/tests/example.test.js
@@ -1,7 +1,11 @@
-import { sum } from "../scripts/Example";
+// import { sum } from "../scripts/Example";
+import {multiply} from '../scripts/Example';
 
 describe('Just an example to testing jest installation', () => {
-    test('should be two', () => {
-        expect(sum(1,1)).toBe(2);
-    });
+    // test('should be two', () => {
+    //     expect(sum(1,1)).toBe(2);
+    // });
+    test('Should be four', ()=>{
+        expect(multiply(2,2)).toBe(4);
+    })
 });
